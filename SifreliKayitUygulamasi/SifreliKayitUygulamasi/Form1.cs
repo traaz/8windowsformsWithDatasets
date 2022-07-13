7b52e91dc0545190c65b5b763e48bd7e14dfb15f
Ali Öztürk <84031734+traaz@users.noreply.github.com>
2022-07-13 17:24:49 +0300

    .

---
+++ SifreliKayitUygulamasi/SifreliKayitUygulamasi/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SifreliKayitUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=TALI;Initial Catalog=SifreliKayit;Integrated Security=True");
+        void sifreliListele()
+        {
+            SqlDataAdapter da=new SqlDataAdapter("Select * From Kisiler",baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        void sifresizListele()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Select * From KisilerSifresiz", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            string ad = textBox1.Text;
+            byte[] adDizi=ASCIIEncoding.ASCII.GetBytes(ad);
+            string adSifre=Convert.ToBase64String(adDizi);
+
+            string soyad = textBox2.Text;
+            byte[] soyadDizi = ASCIIEncoding.ASCII.GetBytes(soyad);
+            string soyadSifre = Convert.ToBase64String(soyadDizi);
+
+            string mail = textBox3.Text;
+            byte[] mailDizi = ASCIIEncoding.ASCII.GetBytes(mail);
+            string mailSifre = Convert.ToBase64String(mailDizi);
+
+            string sifre = textBox4.Text;
+            byte[] sfireDizi = ASCIIEncoding.ASCII.GetBytes(sifre);
+            string sifreSifre = Convert.ToBase64String(sfireDizi);
+
+            string hesapNo = textBox5.Text;
+            byte[] hesapNoDizi = ASCIIEncoding.ASCII.GetBytes(hesapNo);
+            string hesapNoSifre = Convert.ToBase64String(hesapNoDizi);
+
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into Kisiler (Ad,Soyad,Mail,Sifre,HesapNo) values (@P1,@P2,@P3,@P4,@P5)", baglanti);
+            komut.Parameters.AddWithValue("@P1",adSifre);
+            komut.Parameters.AddWithValue("@P2", soyadSifre);
+            komut.Parameters.AddWithValue("@P3", mailSifre);
+            komut.Parameters.AddWithValue("@P4", sifreSifre);
+            komut.Parameters.AddWithValue("@P5", hesapNoSifre);
+
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            sifreliListele();
+           
+            
+            baglanti.Open();
+            SqlCommand komut2 = new SqlCommand("insert into KisilerSifresiz (Ad,Soyad,Mail,Sifre,HesapNo) values (@P1,@P2,@P3,@P4,@P5)", baglanti);
+            komut2.Parameters.AddWithValue("@P1", textBox1.Text);
+            komut2.Parameters.AddWithValue("@P2", textBox2.Text);
+            komut2.Parameters.AddWithValue("@P3", textBox3.Text);
+            komut2.Parameters.AddWithValue("@P4", textBox4.Text);
+            komut2.Parameters.AddWithValue("@P5", textBox5.Text);
+
+            komut2.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kişi eklendi");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sifreliListele();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sifresizListele();
+
+        }
+    }
+}
---
+++ SifreliKayitUygulamasi/SifreliKayitUygulamasi/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("SifreliKayitUygulamasi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SifreliKayitUygulamasi")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("f750104f-5538-4c99-a788-ae30694f671a")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
